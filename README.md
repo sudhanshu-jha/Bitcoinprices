28f0742c0b3965be531866f2e8fbd93f2c955879
Sudhanshu Jha <sudhanshu@bigdrop.io>
2018-12-06 16:49:31 +0530

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Bitcoinprice
-Django web app gets the latest prices 
+Django web app gets the latest prices form api and render to template 
--- bitcoin/views.py
+++ bitcoin/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 import requests
 
 
+# push the data from api to template
 def index(request):
     data = {}
     data["crypto_data"] = get_crypto_data()
     return render(request, "bitcoin/index.html", data)
 
-# return the data received from api as json object
+#  function to return the data received from api as json object
 def get_crypto_data():
-    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=1596"
+    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=2068"
     
     try:
         data = requests.get(api_url).json()
