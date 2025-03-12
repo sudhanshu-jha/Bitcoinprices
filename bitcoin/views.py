f6fd8f0f4563489ac7e9998df426359c47bda48e
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2025-03-12 10:08:11 +0530

    Optimize code in bitcoin/views.py to improve data fetching and error handling

--- bitcoin/views.py
+++ bitcoin/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 import requests
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        logger.info(f"Calling function {func.__name__}")
+        result = func(*args, **kwargs)
+        logger.info(f"Function {func.__name__} finished")
+        return result
+    return wrapper
+
+@index = log_function_call(index)
+@get_crypto_data = log_function_call(get_crypto_data)
+
+
 
 # push the data from api to template
 def index(request):
-    data = {}
+    try:
+        data = get_crypto_data()
+    except Exception as e:
+        print(e)
+        data = dict()
+
     data["crypto_data"] = get_crypto_data()
     return render(request, "bitcoin/index.html", data)
 
-
-#  function to return the data received from api as json object
 
 
 def get_crypto_data():
